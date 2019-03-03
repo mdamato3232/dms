dbce851698eb940d92a968fc4eafefea0073733b
Mike Amato <mikea3232@hotmail.com>
2019-03-03 03:06:23 -0500

    Mar 3, 2019 second one

--- assets/admin.py
+++ assets/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
-from .models import Asset, Components, Sectors
+from .models import Asset, Component, Sector
 
-admin.site.register(Asset)
-admin.site.register(Components)
-admin.site.register(Sectors)
+class AssetAdmin(admin.ModelAdmin):
+  list_display = ('id', 'partnumber', 'serialnumber', 'cbp_component', 'cbp_sector', 'cost')
+  list_display_links = ('id', 'partnumber')
+  list_filter = ('cbp_component', 'cbp_sector')
+  search_fields = ('partnumber', 'cost', 'description')
+
+admin.site.register(Asset, AssetAdmin)
+admin.site.register(Component)
+admin.site.register(Sector)
--- assets/models.py
+++ assets/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-class Components(models.Model):
+class Component(models.Model):
     component = models.CharField(max_length=5)
     def __str__(self):
         return self.component
 
-class Sectors(models.Model):
+class Sector(models.Model):
     sector = models.CharField(max_length=5)
     def __str__(self):
         return self.sector
@@ -16,8 +16,8 @@ class Asset(models.Model):
   partnumber = models.CharField(max_length=200)
   serialnumber = models.CharField(max_length=200)
   description = models.TextField(blank=True)
-  cbp_component = models.ForeignKey(Components,on_delete=models.DO_NOTHING)
-  cbp_sector = models.ForeignKey(Sectors,on_delete=models.DO_NOTHING)
+  cbp_component = models.ForeignKey(Component,on_delete=models.DO_NOTHING)
+  cbp_sector = models.ForeignKey(Sector,on_delete=models.DO_NOTHING)
   acquire_date = models.DateField(blank=True)
   cost = models.IntegerField(blank=True)
   photo_main = models.ImageField(upload_to='photos/%y/%m/%d')
