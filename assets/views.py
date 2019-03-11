b78f7d60be5da065b6c030e18e969b43625e6352
Mike Amato <mikea3232@hotmail.com>
2019-03-11 00:41:44 -0400

    March 11,2019 night

--- assets/views.py
+++ assets/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django_tables2 import RequestConfig
 from .models import Asset
 from .tables import AssetTable
@@ -10,8 +10,13 @@ def index(request):
   RequestConfig(request).configure(table)
   return render(request, 'assets/assets.html', {'table': table})
 
-def asset(request):
-  return render(request, 'assets/asset.html')
+def asset(request, asset_id):
+  asset = get_object_or_404(Asset, pk=asset_id)
+
+  context = {
+    'asset': asset
+  }
+  return render(request, 'assets/asset.html', context)
 
 def search(request):
   return render(request, 'assets/search.html')
