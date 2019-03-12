396406ffafad7cfe31bcdcbeb9ff83e19b235ea9
Mike Amato <mikea3232@hotmail.com>
2019-03-12 01:17:43 -0400

    March 12,2019 started search

--- assets/views.py
+++ assets/views.py
@@ -6,9 +6,16 @@ from .tables import AssetTable
 
 
 def index(request):
-  table = AssetTable(Asset.objects.all())
-  RequestConfig(request).configure(table)
-  return render(request, 'assets/assets.html', {'table': table})
+  if request.method == "GET":
+    print("it's a get"  + request.method)
+    # just present the form
+    return render(request, 'assets/assets.html')
+  else:
+    # read the form, do the query and filtering, render table.
+    print("it's a post" + request.method)
+    table = AssetTable(Asset.objects.all())
+    RequestConfig(request).configure(table)
+    return render(request, 'assets/assets.html', {'table': table})
 
 def asset(request, asset_id):
   asset = get_object_or_404(Asset, pk=asset_id)
