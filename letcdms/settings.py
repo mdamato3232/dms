eb95dc677d4cb587210e0f2983f0a448e7c384a7
Mike Amato <mikea3232@hotmail.com>
2020-10-05 22:34:15 -0400

    Added local settings import and Email config

--- letcdms/settings.py
+++ letcdms/settings.py
@@ -145,3 +145,15 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# Email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER='mdamatodc@gmail.com'
+EMAIL_HOST_PASSWORD=''
+EMAIL_USE_TLS=True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
